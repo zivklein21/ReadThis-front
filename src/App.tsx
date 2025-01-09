90f3d40410c0872bd5cbd66bc94b4e60eda3c322
zivklein21 <zivklein@Mac.lan>
2025-01-09 21:18:46 +0200

    Start

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import React from 'react';
 import './App.css';
-import Login from './components/Login';  // Import the Login component
 
 const App: React.FC = () => {
   return (
     <div className="App">
-      <Login />
+      ReadThis
     </div>
   );
 }
