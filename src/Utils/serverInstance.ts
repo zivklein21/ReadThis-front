be79a93e41ae8aa2a8f31ea180bc6b423a1c21b8
zivklein21 <zivklein@Mac.lan>
2025-01-17 14:05:30 +0200

    remove uneeded files

--- src/Utils/serverInstance.ts
+++
@@ -1,9 +0,0 @@
-// src/utils/axiosInstance.ts
-
-import axios from "axios";
-
-const axiosInstance = axios.create({
-  baseURL: "http://localhost:3000", // or your server's base URL
-});
-
-export default axiosInstance;
