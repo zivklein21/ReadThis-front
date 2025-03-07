5e7a862635b912a07e8b6106e5168f357fb9be68
gefenkidmi <gefenkidmi@gmail.com>
2025-03-07 14:40:22 +0200

    fix add comment

--- src/Utils/api.ts
+++ src/Utils/api.ts
@@ -7,6 +7,7 @@ const api = axios.create({
 // Add token to every request
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem("accessToken"); // Adjust based on your storage mechanism
+  console.log(token);
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
